cb5a13d4c77ca48e88af38b5b5ae7e3150c1ddb0
nxpandroid <srikrishna.pramoda@nxp.com>
2017-03-01 17:39:55 +0530

    [artfxxxx]:SPI version updated to 7.3.0

--- external/libese-spi/p73/inc/phNxpEse_Api.h
+++ external/libese-spi/p73/inc/phNxpEse_Api.h
@@ -73,12 +73,12 @@ typedef struct phNxpEse_initParams
 /*!
  * \brief SEAccess kit MW Major version
  */
-#define ESELIB_MW_VERSION_MAJ                   (0x2)
+#define ESELIB_MW_VERSION_MAJ                   (0x3)
 
 /*!
  * \brief SEAccess kit MW Minor version
  */
-#define ESELIB_MW_VERSION_MIN                   (0x7)
+#define ESELIB_MW_VERSION_MIN                   (0x0)
 
 /******************************************************************************
  * \ingroup spi_libese
