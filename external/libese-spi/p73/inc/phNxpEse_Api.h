b185457ee64136c7db432f1f53395618b2b805da
nxpandroid <srikrishna.pramoda@nxp.com>
2017-04-11 11:12:42 +0530

    0007-artfxxxx-MW-version-updated-to-7.3.6

--- external/libese-spi/p73/inc/phNxpEse_Api.h
+++ external/libese-spi/p73/inc/phNxpEse_Api.h
@@ -78,7 +78,7 @@ typedef struct phNxpEse_initParams
 /*!
  * \brief SEAccess kit MW Minor version
  */
-#define ESELIB_MW_VERSION_MIN                   (0x5)
+#define ESELIB_MW_VERSION_MIN                   (0x6)
 
 /******************************************************************************
  * \ingroup spi_libese
