aaf47cc01db229b95e1cacab2b6a9348bb877e97
nxf24591 <nanjesh.s_1@nxp.com>
2017-11-21 16:30:43 +0530

    SEAccesskit version updated to 8.2.A

--- external/libese-spi/p73/inc/phNxpEse_Api.h
+++ external/libese-spi/p73/inc/phNxpEse_Api.h
@@ -70,7 +70,7 @@ typedef struct phNxpEse_initParams
 /*!
  * \brief SEAccess kit MW Minor version
  */
-#define ESELIB_MW_VERSION_MIN                   (0x09)
+#define ESELIB_MW_VERSION_MIN                   (0x0A)
 
 /******************************************************************************
  * \ingroup spi_libese
